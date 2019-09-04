68faa51c1d59f4bf07d14eeac247ac77f5fa62c8
Zaijianlixiangtl <420991345@qq.com>
2019-09-04 13:37:15 +0800

    linklist

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataStructure.LinkList.Apps;
+
+namespace DataStructure.LinkList.Apps
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var link = new LinkList<Student>();
+			var s1 = new Student() { Id = 1,Name = "lilei"};
+			var s2 = new Student() { Id = 2, Name = "jason" };
+			var s3 = new Student() { Id = 3, Name = "lffffff" };
+			link.Add(s1);
+			link.Add(s2);
+			link.Add(s3);
+			var aa = link.GetAll();
+
+			Console.Read();
+		}
+	}
+}

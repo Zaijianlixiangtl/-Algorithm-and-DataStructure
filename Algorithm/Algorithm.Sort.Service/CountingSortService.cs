6efe387ce8f75a9a73b3914158c41261cc3406a2
Zaijianlixiangtl <420991345@qq.com>
2019-11-07 09:00:16 +0800

    radix

---
+++ Algorithm/Algorithm.Sort.Service/CountingSortService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Sort.Service
+{
+	/// <summary>
+	/// 计数排序
+	/// 属于线性排序  不需要比较  
+	/// 稳定排序算法
+	/// 时间复杂度  O(n)
+	/// </summary>
+	public class CountingSortService : ISortAlgorithmService
+	{
+		public long[] Sort(long[] array)
+		{
+			long[] c = new long[11];
+			long[] r = new long[array.Length];
+			for (int i = 0; i < array.Length; i++)
+			{
+				c[array[i]] += 1;
+			}
+			for (int i = 1; i < c.Length; i++)
+			{
+				c[i] = c[i - 1] + c[i];
+			}
+			for (int i = array.Length-1; i >= 0; i--)
+			{
+				var h = c[array[i]];
+				r[h-1] = array[i];
+				c[array[i]] -= 1;
+			}
+			return r;
+		}
+	}
+}
---
+++ Algorithm/Algorithm.Sort.Service/RadixSortService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.Sort.Service
+{
+	/// <summary>
+	/// 基数排序
+	/// </summary>
+	public class RadixSortService :ISortAlgorithmService
+	{
+		/// <summary>
+		/// 手机号排序
+		/// </summary>
+		/// <param name="array"></param>
+		/// <returns></returns>
+		public long[] Sort(long[] array)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				array = SortOne(array, i);
+			}
+			return array;
+		}
+
+		private long[] SortOne(long[] array, int index)
+		{
+			var j = Math.Pow(10,index);
+			int[] c = new int[10];
+			long[] r = new long[array.Length];
+			for (int i = 0; i < array.Length; i++)
+			{
+				int temp = (int)(array[i]/ j%10);
+				c[temp]++;
+			}
+			for (int i = 1; i < c.Length; i++)
+			{
+				c[i] += c[i - 1];
+			}
+			for (int i = array.Length-1; i >= 0; i--)
+			{
+				int temp = (int)(array[i] / j % 10);
+				r[c[temp]-1] = array[i];
+				c[temp]-=1;
+			}
+			return r;
+		}
+	}
+}
